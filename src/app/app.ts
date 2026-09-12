100e711b31beb67927554bda6e296a8eb18242a9
IEVN1002-23001154 <leonardozepeda703@gmail.com>
2026-09-11 20:48:45 -0600

    HEROES HTML 09/11/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,15 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('introAngular');
+  titulo:string = "Leonardo Hernández"
+  duplicaNumero(num:number):number{
+    return num*2
+  }
+  pelicula={
+    titulo:"El Padrino",
+    anio: 1972,
+    genero: "Crimen/Drama",
+    fechaLanzamiento: new Date(),
+    precio: 345
+  }
 }
