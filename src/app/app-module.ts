38654329735e8c3020957ccfbe986881646bcff7
IEVN1002-23001154 <leonardozepeda703@gmail.com>
2026-09-18 20:58:07 -0600

    NG MODEL 18/09/2026

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,13 +1,21 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
 
 @NgModule({
-  declarations: [App, HeroesList],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    App,
+    HeroesList
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [provideBrowserGlobalErrorListeners()],
-  bootstrap: [App],
+  bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,44 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+  imageWidth: number = 40;
+  imageMargin: number = 2;
+  muestraImage: boolean = true;
+  listFilter: string = '';
+
+  showImage(): void {
+    this.muestraImage = !this.muestraImage;
+  }
+
+  heroes: any[] = [
+    {
+      imagen: 'https://dragonball-api.com/characters/goku_normal.webp',
+      nombre: 'Goku',
+      description: 'Kame H',
+      race: 'Saiyan',
+      ki: '60.000.000'
+    },
+    {
+      imagen: 'https://dragonball-api.com/characters/vegeta_normal.webp',
+      nombre: 'Vegeta',
+      description: '',
+      race: 'Saiyan',
+      ki: '54.000.000'
+    },
+    {
+      imagen: 'https://dragonball-api.com/characters/picolo_normal.webp',
+      nombre: 'Piccolo',
+      description: '',
+      race: 'Namekian',
+      ki: '2.000.000'
+    },
+    {
+      imagen: 'https://dragonball-api.com/characters/gohan.webp',
+      nombre: 'Gohan',
+      description: '',
+      race: 'Human/Saiyan',
+      ki: '50.000.000'
+    }
+  ];
+}
